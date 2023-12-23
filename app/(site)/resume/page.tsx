2a13ea6c7af0c972ab7cad6140e72d3dee82508f
Mohamed El Mesery <107087703+melmesery@users.noreply.github.com>
2023-12-24 00:57:00 +0200

    ssa

--- app/(site)/resume/page.tsx
+++ app/(site)/resume/page.tsx
@@ -1,30 +1,30 @@
-import Education from "@/components/Education.tsx";
-import Languages from "@/components/Languages.tsx";
-import PersonalInfo from "@/components/PersonalInfo.tsx";
-import Profile from "@/components/Profile.tsx";
-import Projects from "@/components/Projects.tsx";
-import SoftSkills from "@/components/SoftSkills.tsx";
-import TechnicalSkills from "@/components/TechnicalSkills.tsx";
+// import Education from "@/components/Education.tsx";
+// import Languages from "@/components/Languages.tsx";
+// import PersonalInfo from "@/components/PersonalInfo.tsx";
+// import Profile from "@/components/Profile.tsx";
+// import Projects from "@/components/Projects.tsx";
+// import SoftSkills from "@/components/SoftSkills.tsx";
+// import TechnicalSkills from "@/components/TechnicalSkills.tsx";
 
-export default function page() {
-  return (
-    <div className="w-[60%] mx-auto grid grid-cols-3 gap-1 py-5">
-      <div className="col-span-1 border border-slate-700 min-h-screen p-5">
-        <PersonalInfo />
-        <hr className="border-slate-700" />
-        <TechnicalSkills />
-        <hr className="border-slate-700" />
-        <SoftSkills />
-        <hr className="border-slate-700" />
-        <Languages />
-      </div>
-      <div className="col-span-2 bg-slate-700 min-h-screen p-5">
-        <Profile />
-        <hr className="border-slate-800" />
-        <Education />
-        <hr className="border-slate-800" />
-        <Projects />
-      </div>
-    </div>
-  );
-}
+// export default function page() {
+//   return (
+//     <div className="w-[60%] mx-auto grid grid-cols-3 gap-1 py-5">
+//       <div className="col-span-1 border border-slate-700 min-h-screen p-5">
+//         <PersonalInfo />
+//         <hr className="border-slate-700" />
+//         <TechnicalSkills />
+//         <hr className="border-slate-700" />
+//         <SoftSkills />
+//         <hr className="border-slate-700" />
+//         <Languages />
+//       </div>
+//       <div className="col-span-2 bg-slate-700 min-h-screen p-5">
+//         <Profile />
+//         <hr className="border-slate-800" />
+//         <Education />
+//         <hr className="border-slate-800" />
+//         <Projects />
+//       </div>
+//     </div>
+//   );
+// }
